d7d81fd11d5157b897f2f41c4f729334bcb154ad
md-mridul-ali <mridulaiub17@gmail.com>
2026-04-12 23:39:59 +0600

    learn Locator concept(getByRole, getByPlaceholder, locate by CSS class, locate using anchor tag)

---
+++ tests/Locators.spec.js
@@ -0,0 +1,34 @@
+// const {test, expect} = require('@playwright/test')
+
+import { test, expect } from '@playwright/test'
+
+test('Locators', async ({ page }) =>{
+    //get the page via url
+    await page.goto('https://localhost:44379/Registration/Index')
+    
+    //click login link using "Xpath"
+    // await page.locator('/html/body/div/form/a').click()
+    await page.getByRole('link', {name: 'Already have an account? Login'}).click()
+
+    //provide user email using "Property"
+    // await page.locator('Email').fill('sakira@gmail.com')
+    // await page.fill('Email', 'sakira@gmail.com')
+    await page.getByPlaceholder('Email').fill('sakira@gmail.com');
+
+    //provide user password using "Property"
+    // await page.locator('Password').fill('123456')
+    // await page.fill('Password', '123456')
+    await page.getByPlaceholder('Password').fill('123456');
+
+    //click on Login button using CSS
+    await page.locator('.btn.btn-success').click()
+
+    //verify About link presence
+    // const aboutLink = await page.locator('/html/body/nav/div/div/ul/li[2]/a')
+    const aboutLink = await page.getByRole('link', {name: 'About'})
+    await expect(aboutLink).toBeVisible()
+
+    await page.close();
+    
+
+})
