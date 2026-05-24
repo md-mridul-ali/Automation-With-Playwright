98ed8d8576e031fc49acd584af949e813386ed24
md-mridul-ali <mridulaiub17@gmail.com>
2026-05-24 23:54:30 +0600

    learn mouse double click action

---
+++ tests/MouseDoubleClickAction.spec.js
@@ -0,0 +1,17 @@
+const {test, expect} = require('@playwright/test')
+
+test('Mouse Double Click', async({page})=>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+
+    const btnCopy = await page.locator('//*[@id="HTML10"]/div[1]/button')
+
+    //double click 
+    await btnCopy.dblclick()
+
+    const f2 = await page.locator('#field2')
+    await expect (f2).toHaveValue('Hello World!')
+
+    await page.waitForTimeout(3000)
+
+})
